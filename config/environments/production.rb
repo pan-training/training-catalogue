7419fc25b69843b24c09eac258519296ae45af1c
root <root@pan-training.hzdr.de>
2021-05-05 10:32:24 +0000

    removed Tess from registration and added sendmail to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -71,14 +71,33 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.default_url_options = {
       host: URI.parse(TeSS::Config.base_url).host,
       port: URI.parse(TeSS::Config.base_url).port,
       protocol: URI.parse(TeSS::Config.base_url).scheme
   }
-  config.action_mailer.asset_host = TeSS::Config.base_url
-  config.action_mailer.smtp_settings = Rails.application.secrets[:smtp] if Rails.application.secrets.key?(:smtp)
+  #config.action_mailer.asset_host = TeSS::Config.base_url
+  #config.action_mailer.smtp_settings = Rails.application.secrets[:smtp] if Rails.application.secrets.key?(:smtp)
+  
+  # ---- sendmail -----
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_options = {from: 'admin@pan-training.hzdr.de'}
+
+  # ---- SMTP -----
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'cg.hzdr.de',
+  #   port:                 465,
+  #   domain:               'hzdr.de',
+  #   user_name:            '',
+  #   password:             '',
+  #   authentication:       'login',
+  #   tls:                  true,
+  #   enable_starttls_auto: true }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
