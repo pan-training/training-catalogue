919a7ea016504931833cfbccdc928f3fded78e78
antoinepado <90184371+antoinepado@users.noreply.github.com>
2021-11-13 01:32:40 +0100

    Update moodle_scraper.rb

--- moodle_scraper.rb
+++ moodle_scraper.rb
@@ -1,6 +1,5 @@
 require 'httparty'
 
-#query_params = {"wstoken" => "6119349a84160e607330feba4cb1299d", "moodlewsrestformat" => "json"}
 query_params = {"wstoken" => "moodle_webservices_token", "moodlewsrestformat" => "json"}
 
 #query_params_to_merge=query_params.merge({"wsfunction" => "core_webservice_get_site_info"})
