285f963aab1b17d6e6ffd936b332ede4ee4c5ae9
Oliver Knodel <o.knodel@hzdr.de>
2022-03-07 15:48:40 +0100

    Umbrella login for new unknown users possible

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,9 +2,11 @@ class CallbacksController < Devise::OmniauthCallbacksController
 
   def umbrella_aai
     @user = User.from_omniauth(request.env["omniauth.auth"])
-
+    
+    puts @user
+    
     if @user.new_record?
-      @user.save
+      @user.save(validate: false)
       sign_in @user
       flash[:notice] = "#{I18n.t('devise.registrations.signed_up')} Please ensure your profile is correct."
       redirect_to edit_user_path(@user)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug 
+  config.log_level = :error 
   #:debug
 
   # Prepend all log lines with the following tags.
