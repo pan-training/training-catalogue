636fe72332b4b08bb2d4afed6cc93f816ad055d1
Oliver Knodel <o.knodel@hzdr.de>
2022-03-10 11:40:54 +0100

    changed urls to pan-training.eu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :error 
+  config.log_level = :warn
   #:debug
 
   # Prepend all log lines with the following tags.
